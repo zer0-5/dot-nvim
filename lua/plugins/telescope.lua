6231d67e38d2ef5863534be092f7da381dd48bcc
zer0-5 <guilherme2001fernandes@hotmail.com>
2023-07-27 13:24:42 +0100

    Add telescope icons plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-file-browser.nvim",
             "debugloop/telescope-undo.nvim",
+            'nvim-telescope/telescope-symbols.nvim',
         },
         cmd = "Telescope",
         keys = {

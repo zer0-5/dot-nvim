4448f17ed72f0686ffed608495725ecb20bf4de7
zer0-5 <guilherme2001fernandes@hotmail.com>
2023-08-07 16:47:04 +0100

    Show file relative path and icons in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,10 +3,13 @@ return {
     event = { "BufRead", "BufNewFile", "VimEnter" },
     opts = {
         options = {
-            icons_enabled = false,
+            icons_enabled = true,
             theme = "auto",
             component_separators = "|",
             section_separators = "",
+        },
+        sections = {
+            lualine_c = { { 'filename', path = 1 } },
         },
     },
 }
